25609e5c9d2bf80f0896686d97bf6aea9f2b256d
Gakiri001 <gakirijulius@gmail.com>
2024-12-24 12:57:45 +0300

    server set up and database connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
-import { Button } from './components/ui/button'
+import { Button } from "./components/ui/button";
 
 function App() {
-
   return (
     <>
-    <Button className="bg-red-600">Check</Button>
+      <Button className="bg-red-600">Check</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ server/server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+cors({
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+});
+
+//Handling parsing and middleware
+app.use(express.json);
+
+//database connection
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB is connected"))
+  .catch((e) => console.log(e));
+
+//Global Error handler
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({ success: false, message: "Something went wrong" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
