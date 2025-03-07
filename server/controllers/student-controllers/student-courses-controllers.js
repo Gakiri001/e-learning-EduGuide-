a2b899efc6805dd02b725acb6a57073d9142df2c
Gakiri001 <gakirijulius@gmail.com>
2025-03-07 11:01:52 +0300

    clearing log

--- server/controllers/student-controllers/student-courses-controllers.js
+++ server/controllers/student-controllers/student-courses-controllers.js
@@ -11,14 +11,11 @@ const getCoursesByStudentID = async (req, res) => {
       success: true,
       data: studentBoughtCourses.courses || [],
     });
-    console.log("Fetched Student Courses: ", studentBoughtCourses);
-    console.log("kkkk");
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: "some Error Occured" });
   }
 };
 
-console.log("kkkk");
 
 module.exports = { getCoursesByStudentID };
