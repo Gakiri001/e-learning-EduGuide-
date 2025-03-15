eed66a0313f3de7d6019071cbadc6aa1546f0a2b
Gakiri001 <gakirijulius@gmail.com>
2025-03-15 15:09:22 +0300

    course progress, overview & course content

--- client/src/Pages/student/course-progress/index.jsx
+++ client/src/Pages/student/course-progress/index.jsx
@@ -1,9 +1,21 @@
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import VideoPlayer from "@/components/video-player";
 import { AuthContext } from "@/Context/Auth-context";
 import { studentContext } from "@/Context/student-context";
 import { getCurrentCourseProgressService } from "@/services";
-import { ChevronLeft } from "lucide-react";
-import React, { useContext, useEffect } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import React, { useContext, useEffect, useState } from "react";
+import ReactConfetti from "react-confetti";
 import { useNavigate, useParams } from "react-router-dom";
 
 function StudentViewCourseProgressPage() {
@@ -12,6 +24,12 @@ function StudentViewCourseProgressPage() {
 
   const { studentCurrentCourseProgress, setStudentCurrentCourseProgress } =
     useContext(studentContext);
+  const [lockCourse, setLockCourse] = useState(false);
+  const [currentLecture, setCurrentLecture] = useState(null);
+  const [showCourseCompletedDialog, setShowCourseCompletedDialog] =
+    useState(false);
+  const [showConnfetti, setShowConfetti] = useState(false);
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
 
   const courseid = useParams();
   console.log("Courseid", courseid.id);
@@ -23,15 +41,49 @@ function StudentViewCourseProgressPage() {
       courseid.id,
     );
 
-    console.log("response", response);
+    // console.log("response", response?.data);
+
+    if (response?.success) {
+      if (!response?.data?.isPurchased) {
+        setLockCourse(true);
+      } else {
+        setStudentCurrentCourseProgress({
+          courseDetails: response?.data?.courseDetails,
+          progress: response?.data?.progress,
+        });
+        if (response?.data?.completed) {
+          setCurrentLecture(response?.data?.courseDetails?.curriculum[0]);
+          setShowCourseCompletedDialog(true);
+          setShowConfetti(true);
+
+          return;
+        }
+
+        if (response?.data?.progress?.length === 0) {
+          console.log("response", response?.data);
+          setCurrentLecture(response?.data?.courseDetails?.curriculum[0]);
+        } else {
+          //later
+        }
+      }
+    }
   }
+
+  console.log("studentCurrentCourseProgress", studentCurrentCourseProgress);
 
   useEffect(() => {
     fetchCurrentCourseProgress();
   }, [courseid]);
 
+  useEffect(() => {
+    if (showConnfetti) setTimeout(() => setShowConfetti(false), 10000);
+  }, [showConnfetti]);
+
+  console.log("currentLecture", currentLecture);
+
   return (
     <div className="flex flex-col h-screen bg-[#1c1d1f] text-white">
+      {showConnfetti && <ReactConfetti />}
       <div className="flex justify-between items-center p-4 bg-[#1c1d1f] border-b border-gray-700">
         <div className="flex items-center space-x-2">
           <Button
@@ -43,9 +95,105 @@ function StudentViewCourseProgressPage() {
             <ChevronLeft className="h-4 w-4 mr-2" />
             Back to My Courses Page
           </Button>
-          <h1>Oyah Mzee</h1>
+          <h1 className="text-lg font-bold hidden md:block">
+            {studentCurrentCourseProgress?.courseDetails?.title}
+          </h1>
+        </div>
+        <Button onClick={() => setIsSideBarOpen(!isSideBarOpen)}>
+          {isSideBarOpen ? (
+            <ChevronRight className="h-5 w-5" />
+          ) : (
+            <ChevronLeft className="h-5 w-5" />
+          )}
+        </Button>
+      </div>
+      <div className="flex flex-1 overflow-hidden">
+        <div
+          className={`flex-1 ${isSideBarOpen ? "mr-[400px]" : ""} translation-all duration-300`}
+        >
+          <VideoPlayer
+            width="100%"
+            height="500px"
+            url={currentLecture?.videoUrl}
+            onProgressUpdate={setCurrentLecture}
+            progressData={currentLecture}
+          />
+          <div className="p-6 bg-[#1c1d1f]">
+            <h2 className="text-2xl font-bold mb-2">{currentLecture?.title}</h2>
+          </div>
+        </div>
+        <div
+          className={`fixed top-[64px] right-0 bottom-0 w-[400px] bg-[#1c1d1f] border-l border-gray-700 translation-all duration-300 ${isSideBarOpen ? "translate-x-0" : "translate-x-full"}`}
+        >
+          <Tabs defaultValue="content" className="h-full flex flex-col">
+            <TabsList className="grid bg-[#535353] w-full grid-cols-2 p-0 h-14">
+              <TabsTrigger
+                value="content"
+                className="text-black rounded-none h-full"
+              >
+                Course Content
+              </TabsTrigger>
+              <TabsTrigger
+                value="overview"
+                className="text-black rounded-none h-full"
+              >
+                Overview
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="content">
+              <ScrollArea className="h-full">
+                <div className="p-4 space-y-4">
+                  {studentCurrentCourseProgress?.courseDetails?.curriculum.map(
+                    (item) => (
+                      <div
+                        className="flex items-center space-x-2 text-sm text-white font-bold cursor-pointer"
+                        key={item._id}
+                      >
+                        <span>{item?.title}</span>
+                      </div>
+                    ),
+                  )}
+                </div>
+              </ScrollArea>
+            </TabsContent>
+            <TabsContent value="overview" className="flex-1 overflow-hidden">
+              <ScrollArea className="h-full">
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-4">About This Course</h2>
+                  <p className="text-gray-400">
+                    {studentCurrentCourseProgress?.courseDetails?.description}
+                  </p>
+                </div>
+              </ScrollArea>
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
+      <Dialog open={lockCourse}>
+        <DialogContent className="sm:w-[425px]">
+          <DialogHeader>
+            <DialogTitle>You can't view this page</DialogTitle>
+            <DialogDescription>
+              Please purchase this course to get access
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog open={showCourseCompletedDialog}>
+        <DialogContent className="sm:w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Congratulations!!!</DialogTitle>
+            <DialogDescription className="flex flex-col gap-3">
+              <Label>You have Completed the Course</Label>
+              <div className="flex gap-4">
+                <Button>My Course Page</Button>
+                <Button>Rewatch this Course</Button>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
